61c8078113e80d3218f23765176683aa4a4a1aae
clemens2001 <s2210307114@fhooe.at>
2024-10-16 09:49:18 +0200

    UE03_Begin

---
+++ SWK_Softwareentwicklung/UE03/TimerDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿
+void LogTimerExpiration()
+{
+    Console.WriteLine("Timer elapsed!!!");
+}
+
+var timer = new TimerDemo.Timer();
+
+timer.Interval = 500;
+
+timer.Expired = LogTimerExpiration;
+timer.Expired = () => Console.WriteLine("Timer (lambda) elapsed");
+
+timer.Start();
---
+++ SWK_Softwareentwicklung/UE03/TimerDemo/Timer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimerDemo
+{
+    public delegate void ExpiredEventHandler();
+    public class Timer
+    {
+
+        private const int DEFAULT_INTERVAL = 1000;
+        private readonly Thread thread;
+
+        public Timer()
+        {
+            thread = new Thread(OnTick);
+        }
+
+        public int Interval { get; set; } = DEFAULT_INTERVAL;
+        
+        public ExpiredEventHandler? Expired { get; set; }
+
+        public void Start() => thread.Start();
+
+        private void OnTick()
+        {
+            while (true) {
+                Thread.Sleep(Interval);
+
+                // Console.WriteLine("Timer elapsed!!!");
+                Expired?.Invoke();
+            }
+        }
+    }
+}
