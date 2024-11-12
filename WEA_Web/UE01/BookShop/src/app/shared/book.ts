759c22bfd7f03c0f4ba9d4a7157759fb43d9b6ec
clemens2001 <s2210307114@fhooe.at>
2024-11-12 09:06:02 +0100

    wea5 ue01 booklist

---
+++ WEA_Web/UE01/BookShop/src/app/shared/book.ts
@@ -0,0 +1,16 @@
+export class Book {
+
+    //public id: string;
+    
+    constructor(
+        public id?: string,
+        public author?: string,
+        public title?: string,
+        public price?: string,
+        public publisher?: string,
+        public year?: string,
+        public description?: string,
+        public picture?: string,
+        public isbn?: string,
+    ) { }
+}
