db02ecc0fba347c728d6811cac34a8c88929ca57
clemens2001 <s2210307114@fhooe.at>
2024-11-22 12:20:48 +0100

    add canteen checker program

---
+++ SMA_Android/app/src/main/java/at/fhooe/se/canteencheckerconsumerapp/ui/CanteensActivity.kt
@@ -0,0 +1,21 @@
+package at.fhooe.se.canteencheckerconsumerapp.ui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import at.fhooe.se.canteencheckerconsumerapp.R
+
+class CanteensActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
