65d3bb6dcb9eb9af842587db1f91277c2bc8dd2b
clemens2001 <s2210307114@fhooe.at>
2024-12-11 10:07:07 +0100

    add initial wpf project

---
+++ SWK_Softwareentwicklung/UE08/Swack/Swack.UI/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Swack.Logic.Models;
+using Swack.UI.ViewModels;
+
+namespace Swack.UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public IEnumerable<Channel> Channels { get; private set; }
+
+        public Channel? CurrentChannel { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var viewModel = new MainViewModel();
+
+            DataContext = viewModel;
+        }
+    }
+}
---
+++ SWK_Softwareentwicklung/UE08/Swack/Swack.UI/ViewModels/MainViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Swack.Logic.Models;
+
+namespace Swack.UI.ViewModels
+{
+    public class MainViewModel
+    {
+        public IEnumerable<Channel> Channels { get; set; }
+
+        // ctor TAB
+        public MainViewModel()
+        {
+            Channels = new List<Channel>()
+            {
+                new("#swk"),
+                new("#fun"),
+                new("#kurztest"),
+            };
+        }
+
+    }
+}
