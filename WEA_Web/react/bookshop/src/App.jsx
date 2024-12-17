09291ed7cb1a9b173658e9726b634d4213815b3b
clemens2001 <s2210307114@fhooe.at>
2024-12-17 12:16:26 +0100

    bookdetails

--- WEA_Web/react/bookshop/src/App.jsx
+++ WEA_Web/react/bookshop/src/App.jsx
@@ -1,11 +1,41 @@
+import React from 'react';
 import './App.css';
 import BookList from './components/BookList';
+import BookDetails from "./components/BookDetails.jsx";
 
-function App() {
+class App extends React.Component {
 
-  return (
-    <BookList onBookClick={(book) => alert(book.title)}/>
-  );
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedBook: undefined
+    }
+  }
+
+  showDetails(book) {
+    this.setState({ selectedBook: book });
+  }
+
+  showList() {
+      this.setState({ selectedBook: undefined });
+  }
+
+  render() {
+    let content;
+    if (!this.state.selectedBook) {
+      // content = <BookList onBookClick={this.showDetails.bind(this)}/>;  // .bind because we want to pass the context (otherwise this will be undefined)
+      content = <BookList onBookClick={(book) => this.showDetails(book)}/>;
+    } else {
+      // content = <h1>Details of the book {this.state.selectedBook.title}</h1>
+      content = <BookDetails book={this.state.selectedBook} onBackClick={() => this.showList()} />;
+    }
+
+    return (
+        <div className="ui container">
+            {content}
+        </div>
+    );
+  }
 }
 
 export default App
---
+++ WEA_Web/react/bookshop/src/components/BookDetails.jsx
@@ -0,0 +1,37 @@
+﻿
+
+
+function BookDetails(props) {
+
+    const { book, onBackClick } = props;
+
+    return (
+        <div>
+            <div className="ui padded grid">
+                <div className="eight wide column">
+                    <h1 className="ui header">{book.title}</h1>
+                    <h3 className="ui header">{book.author}</h3>
+                    <div className="ui divider"></div>
+                    <div className="ui grid">
+                        <div className="four wide column">
+                            <h4 className="ui header">Preis</h4>
+                            {book.price} €
+                        </div>
+                        <div className="four wide column">
+                            <h4 className="ui header">Erschienen</h4>
+                            {book.year}
+                        </div>
+                    </div>
+                </div>
+                <div className="sixteen wide column">
+                    <h3 className="ui header">Beschreibung</h3>
+                    {book.description}
+                </div>
+
+            </div>
+            <button className="ui red button" onClick={onBackClick}>Zurück zur Buchliste</button>
+        </div>
+    );
+}
+
+export default BookDetails;
